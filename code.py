ef82738be26622139697a71d4ee2bdb19bb5e2af
ben-whitten <54954680+ben-whitten@users.noreply.github.com>
2019-10-30 12:43:36 -0400

    Update and rename bitch to code.py

---
+++ code.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+
+# Created by: Ben Whitten
+# Created on: October 2019
+# Makes a sprite move on circuit python
+
+import ugame
+import stage
+import constants
+
+image_bank_1 = stage.Bank.from_bmp16("ball.bmp")
+sprites = []
+
+
+def main():
+    # this sets the background
+    a_button = constants.button_state["button_up"]
+    b_button = constants.button_state["button_up"]
+    start_button = constants.button_state["button_up"]
+    select_button = constants.button_state["button_up"]
+
+    pew_sound = open("pew.wav", 'rb')
+    sound = ugame.audio
+    sound.stop()
+    sound.mute(False)
+    background = stage.Grid(image_bank_1, 10, 8)
+
+    # create a sprite
+    # parameters (image_bank_1, image # in bank, x, y)
+    ball_one = stage.Sprite(image_bank_1, 3, 64, 56)
+    sprites.append(ball_one)
+    ball_two = stage.Sprite(image_bank_1, 2, 75, 56)
+    sprites.insert(0, ball_two)
+
+
+    game = stage.Stage(ugame.display, constants.FPS)
+    game.layers = sprites + [background]
+
+    game.render_block()
+
+    while True:
+        # get user inputs
+        keys = ugame.buttons.get_pressed()
+        # print(keys)
+        if keys & ugame.K_X != 0:
+            if a_button == constants.button_state["button_up"]:
+                a_button = constants.button_state["button_just_pressed"]
+            elif a_button == constants.button_state["button_just_pressed"]:
+                a_button = constants.button_state["button_still_pressed"]
+        else:
+            if a_button == constants.button_state["button_still_pressed"]:
+                a_button = constants.button_state["button_released"]
+            else:
+                a_button = constants.button_state["button_up"]
+        if keys & ugame.K_UP != 0:
+            if ball_one.y < 0:
+                ball_one.move(ball_one.x, 0)
+            else:
+                ball_one.move(ball_one.x, ball_one.y - constants.SPRITE_MOVEMENT_SPEED)
+            pass
+        if keys & ugame.K_DOWN != 0:
+            if ball_one.y > constants.SCREEN_Y - constants.SCREEN_GRID_Y:
+                ball_one.move(ball_one.x, constants.SCREEN_Y -
+                              constants.SPRITE_SIZE)
+            else:
+                ball_one.move(ball_one.x, ball_one.y + constants.SPRITE_MOVEMENT_SPEED)
+            pass
+        if keys & ugame.K_LEFT != 0:
+            if ball_one.x < 0:
+                ball_one.move(0, ball_one.y)
+            else:
+                ball_one.move(ball_one.x - constants.SPRITE_MOVEMENT_SPEED, ball_one.y)
+            pass
+        if keys & ugame.K_RIGHT != 0:
+            if ball_one.x > constants.SCREEN_X - constants.SCREEN_GRID_X:
+                ball_one.move(constants.SCREEN_X - constants.SPRITE_SIZE,
+                              ball_one.y)
+            else:
+                ball_one.move(ball_one.x + constants.SPRITE_MOVEMENT_SPEED, ball_one.y)
+            pass
+        # update game logic
+        if a_button == constants.button_state["button_just_pressed"]:
+            sound.play(pew_sound)
+
+        # redraw sprtie list
+        game.render_sprites(sprites)
+        game.tick()  # wait until refresh rate finishes
+
+
+if __name__ == "__main__":
+    main()
